936d424eff3f11800d0b326e78069936335022a2
Duvier Reinosa <55174272+Duvier-Reinosa@users.noreply.github.com>
2021-03-01 17:51:19 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react';
+
+
+
+import HomeStack from "./App/navigation/HomeStack";
+
+
+export default function App() {
+  
+    return <HomeStack/>
+      }
+
